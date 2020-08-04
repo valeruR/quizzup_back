dcfd9a7f39ce0b4bf287188295e25bf2dd7bb84d
Reever Valerus <reever.valerus@epitech.eu>
2020-08-04 14:41:29 +0200

    access-control

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+
+app.use(bodyParser.json()); // application/json
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 mongoose
 .connect(
